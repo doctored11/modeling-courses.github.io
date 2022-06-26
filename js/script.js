0af7fbed063ce736fa1f3c0cdfd9f04f22f10430
drEd11 <93774837+doctored11@users.noreply.github.com>
2022-06-26 22:00:52 +0300

    full adaptive

---
+++ js/script.js
@@ -0,0 +1,22 @@
+let burger = document.querySelector('.header__burger-ico');
+let menu = document.querySelector('.header__drop-down-menu ');
+let menuLinks = menu.querySelectorAll('.header__menu');
+// burger
+burger.addEventListener('click',
+    function() {
+        burger.classList.toggle('header__burger-ico--cross');
+        menu.classList.toggle('header__drop-down-menu--active');
+
+        document.body.classList.toggle('stop-scroll');
+    }
+);
+
+menuLinks.forEach(function(el) {
+    el.addEventListener('click', function() {
+        burger.classList.remove('header__burger-ico--cross');
+        menu.classList.toggle('header__drop-down-menu--active');
+        document.body.classList.remove('stop-scroll');
+
+
+    })
+});
