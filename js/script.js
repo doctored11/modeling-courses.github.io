bbdda410bc8d30f3b9e7863ab3d85e219a1dd363
drEd11 <93774837+doctored11@users.noreply.github.com>
2022-06-28 00:48:54 +0300

    add default student page

--- js/script.js
+++ js/script.js
@@ -20,3 +20,25 @@ menuLinks.forEach(function(el) {
 
     })
 });
+// 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,},
+
+
+
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false
+    },
+});
